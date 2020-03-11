64e8af3cb8e1fc3b209de0febdd582eb04dfcac3
rudra0 <prudra882@gmail.com>
2020-03-12 00:28:57 +0530

    Updates

---
+++ precache-manifest.dc44de39a71d36776c6e3e3610bcd83f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11daa02c1c843a9d1ea7c7e349476275",
+    "url": "/index.html"
+  },
+  {
+    "revision": "246c1fc72f5c1620df76",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "50ae54ab61f9f5e90d37",
+    "url": "/static/js/2.0b98a6c5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.0b98a6c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "246c1fc72f5c1620df76",
+    "url": "/static/js/main.e45e836f.chunk.js"
+  },
+  {
+    "revision": "00c97b8943990c4f5c07",
+    "url": "/static/js/runtime-main.bb588c0d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
